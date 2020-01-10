4e4de3e9516d54c04b546c1a98125c03bb7db4d6
Heidi <hspradlin8@gmail.com>
2020-01-10 15:46:11 -0600

    updated the try catch

--- DepartmentEmployeeMVC/Models/Employee.cs
+++ DepartmentEmployeeMVC/Models/Employee.cs
@@ -11,7 +11,7 @@ namespace DepartmentEmployeeMVC.Models
         public int Id { get; set; }
         [Display(Name = "First Name")]
         [Required]
-        [MaxLength(15, ErrorMessage = "First Name must be less than 15 characters")]
+        [MaxLength(15, ErrorMessage = "First Name must be less than 16 characters")]
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
